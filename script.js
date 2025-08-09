d4f044a2dbeae34add2f6e6b1324f15de9bcf4c3
Hernan Gonzalez <hernangozalez@gmail.com>
2025-08-09 10:19:20 -0300

    Primeras pruebas de fondo animado

---
+++ script.js
@@ -0,0 +1,14 @@
+
+VANTA.CELLS({
+  el: "#fondo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  size: 5,
+  speed: 0.1,
+  color1: 0x000c33,
+  color2: 0x0000,
+}) 
